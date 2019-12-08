ce1140dcc038c8e3734c07e18d993f41efc0d2b1
Rafael Sachetto <rafael@sachetto.com.br>
2019-12-08 17:19:44 -0300

    Initial Commit

---
+++ Experiments With SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Experiments With SwiftUI
+//
+//  Created by Rafael Sachetto on 08/12/19.
+//  Copyright © 2019 Rafael Sachetto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Experiments With SwiftUITests/Experiments_With_SwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  Experiments_With_SwiftUITests.swift
+//  Experiments With SwiftUITests
+//
+//  Created by Rafael Sachetto on 08/12/19.
+//  Copyright © 2019 Rafael Sachetto. All rights reserved.
+//
+
+import XCTest
+@testable import Experiments_With_SwiftUI
+
+class Experiments_With_SwiftUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
